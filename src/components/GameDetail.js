db2e6cad31f7410d51d203d75aad2d60cacb4501
nggar <enggarjpx@gmail.com>
2022-08-03 01:03:54 +0700

    style GameDetail Component

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -10,28 +10,28 @@ const GameDetail = () => {
     return (
         <CardShadow>
             <Detail>
-                <div className="stats">
+                <Stats>
                     <div className="rating">
                         <h3>{game.name}</h3>
                         <p>Rating: {game.rating}</p>
                     </div>
-                    <div className="info">
+                    <Info>
                         <h3>Platforms</h3>
-                        <div className="platforms">
+                        <Platforms>
                             {game.platforms.map((platform) => (
                                 <h3 key={platform.platform.id}>
                                     {platform.platform.name}
                                 </h3>
                             ))}
-                        </div>
-                    </div>
-                </div>
-                <div className="media">
+                        </Platforms>
+                    </Info>
+                </Stats>
+                <Media>
                     <img src={game.background_image} alt={game.name} />
-                </div>
-                <div className="description">
+                </Media>
+                <Description>
                     <p>{game.description_raw}</p>
-                </div>
+                </Description>
                 <div className="gallery">
                     {screen.results.map((screen) => (
                         <img
@@ -50,7 +50,7 @@ const CardShadow = styled(motion.div)`
     width: 100%;
     height: 100vh;
     overflow-y: scroll;
-    background-color: rgba(0, 0, 0, 0.4);
+    background-color: rgba(0, 0, 0, 0.5);
 
     position: fixed;
     top: 0;
@@ -60,7 +60,7 @@ const CardShadow = styled(motion.div)`
 const Detail = styled(motion.div)`
     width: 80%;
     border-radius: 1rem;
-    padding: 2rem 10rem;
+    padding: 2rem 5rem;
     background-color: white;
 
     position: absolute;
@@ -70,6 +70,38 @@ const Detail = styled(motion.div)`
     img {
         width: 100%;
     }
+`;
+
+const Stats = styled(motion.div)`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Info = styled(motion.div)`
+    text-align: center;
+`;
+
+const Platforms = styled(motion.div)`
+    display: flex;
+    justify-content: space-evenly;
+
+    img {
+        margin-left: 3rem;
+    }
+`;
+
+const Media = styled(motion.div)`
+    margin-top: 5rem;
+
+    img {
+        width: 100%;
+        object-fit: cover;
+    }
+`;
+
+const Description = styled(motion.div)`
+    margin: 5rem 0rem;
 `;
 
 export default GameDetail;
--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -30,7 +30,7 @@ const GlobalStyle = createGlobalStyle`
 
     h3 {
         font-size: 1.3rem;
-        padding: 1.5rem;
+        padding: 1.5rem 0rem;
         color: #333;
     }
 
